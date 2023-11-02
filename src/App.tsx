44874a9ba58a38661eb5845f4ba6c4114a5a0081
Jan Steikowski <j.steikowski@aol.com>
2023-11-02 17:49:22 +0700

    Delete unneccessary styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import styles from "./styles/test.module.scss";
+import styles from "./styles/App.module.scss";
 function App() {
   const [count, setCount] = useState(0);
 
