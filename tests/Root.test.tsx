8cf3a477eb47ba1c26b0d5451661a9335ab4ade1
Jan Steikowski <j.steikowski@aol.com>
2023-11-04 17:45:09 +0700

    Setup root test

---
+++ tests/Root.test.tsx
@@ -0,0 +1,12 @@
+/*
+import { render, screen } from '@testing-library/react';
+import Root from '../src/routes/Root';
+import React from 'react';
+*/
+
+describe('Root component', () => {
+  it('renders Root component'), () => {
+    // do tests
+  }
+})
+
