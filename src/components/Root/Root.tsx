a36ed7e431e16458779bc917fefd58075ae01f88
Jan Steikowski <j.steikowski@aol.com>
2023-11-20 16:29:31 +0700

    Add text to root

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -7,7 +7,16 @@ const Root = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <div></div>
+      <div className={styles.rootContainer}>
+        <section className={styles.greeting}>
+          <h2>Meet your favorite character!</h2>
+          <p>
+            Have you ever thought about how cool it would be to drink a coffee with Mr. Kakapoopoo or go into another
+            crazy universe together with Rick and Morty? Well, you can stop dreaming! Here in the Schwifty Shop, you can
+            buy time with every single character from Rick and Morty and spend it however you want!
+          </p>
+        </section>
+      </div>
     </div>
   );
 };
