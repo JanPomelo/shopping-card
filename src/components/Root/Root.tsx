f8039ab1cf6803f988b1d0f3e77c5503272a2c30
Jan Steikowski <j.steikowski@aol.com>
2023-11-20 11:11:48 +0700

    Add some styles to root

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -7,8 +7,19 @@ const Root = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <div></div>
-      <div></div>
+      <div className={styles.rootContainer}>
+        <section className={styles.greeting}>
+          <h2>Welcome to the Book Buffet</h2>
+          <p>
+            In this shop, you'll find every book for just 2 Euro. Why? Because we think that reading should be
+            affordable for everyone. So, have fun browsing through our shop!
+          </p>
+        </section>
+        <section className={styles.items}>
+          <h3>Newest Arrivals</h3>
+          <div></div>
+        </section>
+      </div>
     </div>
   );
 };
