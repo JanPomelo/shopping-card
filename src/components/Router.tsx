046aa2997e4dcf04ff2492ecef0d11c8acd017ae
Jan Steikowski <j.steikowski@aol.com>
2023-11-21 11:53:52 +0700

    Add Terms and conditions Page

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,6 +2,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./ErrorPage/Errorpage";
 import Root from "./Root/Root";
+import Terms from "./Terms/Terms";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -9,10 +10,16 @@ const Router = () => {
       path: "/",
       element: <Root />,
       errorElement: <ErrorPage />,
-    },
-    {
-      path: "shop",
-      element: <Root />,
+      children: [
+        {
+          path: "shop",
+          element: <></>,
+        },
+        {
+          path: "Terms-and-conditions",
+          element: <Terms />,
+        },
+      ],
     },
   ]);
 
---
+++ src/components/Terms/Terms.tsx
@@ -0,0 +1,7 @@
+"use strict";
+
+const Terms = () => {
+  return <></>;
+};
+
+export default Terms;
