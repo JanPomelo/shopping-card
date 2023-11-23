bb65fbdf6feedeb1ee061b4effc8a1a2385087d7
Jan Steikowski <j.steikowski@aol.com>
2023-11-23 11:15:59 +0700

    Add special offer display on cards

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -3,16 +3,19 @@
 import styles from "./CharacterCard.module.scss";
 import CharacterCardProps from "./CharacterCardProps";
 
-const CharacterCard: React.FC<CharacterCardProps> = ({ char, price }) => {
+const CharacterCard: React.FC<CharacterCardProps> = ({ char, price, sales }) => {
   return (
     <div className={styles.container}>
+      {sales ? <div className={styles.specialOffer}></div> : <></>}
       <img src={char.image}></img>
       <div className={styles.nameDiv}>
         <h3>{char.name}</h3>
       </div>
       <div className={styles.informations}>
         <p>Price:</p>
-        <p>{price} <span className={styles.flurbo}></span></p>
+        <p>
+          {price} <span className={styles.flurbo}></span>
+        </p>
       </div>
     </div>
   );
--- src/components/CharacterCard/CharacterCardProps.ts
+++ src/components/CharacterCard/CharacterCardProps.ts
@@ -3,6 +3,7 @@ import Character from "../../types/character";
 interface CharacterCardProps {
   char: Character;
   price: number;
+  sales: boolean;
 }
 
 export default CharacterCardProps;
