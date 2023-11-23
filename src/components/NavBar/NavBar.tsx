0f843d01abfbe80990e45958667913932d8512d1
Jan Steikowski <j.steikowski@aol.com>
2023-11-23 14:46:18 +0700

    Add arrow to hint to shop

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,17 +5,18 @@ import { Link } from "react-router-dom";
 styles;
 
 const NavBar: React.FC<NavBarProps> = ({ onButtonClick, page }) => {
-  return (
-    page === "Shop" ? (
-      <Link to="/" onClick={onButtonClick} className={styles.link}>
-        Home
-      </Link>
-    ) : (
+  return page === "Shop" ? (
+    <Link to="/" onClick={onButtonClick} className={styles.link}>
+      Home
+    </Link>
+  ) : (
+    <div className={styles.container}>
+      <div className={styles.arrow}></div>
       <Link to="shop" onClick={onButtonClick} className={styles.link}>
         Shop
       </Link>
-    )
-  )
+    </div>
+  );
 };
 
 export default NavBar;
