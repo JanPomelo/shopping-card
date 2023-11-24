763b0704509c3bc7f12e64363fbace25fbf8b688
Jan Steikowski <j.steikowski@aol.com>
2023-11-24 13:06:42 +0700

    Add new properties to character type

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,11 +1,14 @@
 type Character = {
   name: string;
   species: string;
+  gender: string;
   origin: {
     name: string;
-  },
+  };
   image: string;
   id: number;
-}
+  status: string;
+  episode: string[];
+};
 
 export default Character;
