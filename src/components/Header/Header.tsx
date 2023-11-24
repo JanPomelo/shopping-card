7b0716926e629cef75ecfbc95670fa006f3304ed
Jan Steikowski <j.steikowski@aol.com>
2023-11-24 13:05:39 +0700

    fix image load problem in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ styles;
 const Header: React.FC<HeaderProps> = ({ page, onClick }) => {
   return (
     <div className={styles.header}>
-      <img src="./rick.png" alt="Rick" />
+      <img src="/rick.png" alt="Rick" />
       <h1>Schwifty Shop</h1>
       <NavBar
         page={page}
