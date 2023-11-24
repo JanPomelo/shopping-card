7829c57e5219e586e43ce1244d46654be4c845a5
Jan Steikowski <j.steikowski@aol.com>
2023-11-24 13:05:26 +0700

    Add dynamic item route to router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -5,21 +5,26 @@ import Root from "./Root/Root";
 import Terms from "./Terms/Terms";
 import Faq from "./FAQ/Faq";
 import Home from "./Home/Home";
+import Item from "./Item/Item";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Root/>,
+      element: <Root />,
       errorElement: <ErrorPage />,
       children: [
         {
-          path: '/',
-          element: <Home/>,
+          path: "/",
+          element: <Home />,
         },
         {
           path: "shop",
           element: <></>,
+        },
+        {
+          path: "/shop/:id",
+          element: <Item />,
         },
         {
           path: "Terms-and-conditions",
