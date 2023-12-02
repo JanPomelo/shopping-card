d7b74f7b6724484c77a1a8ebd2dcba3b8ebefe56
Jan Steikowski <j.steikowski@aol.com>
2023-12-02 11:19:04 +0700

    Add blur to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,14 @@ import styles from "./Card.module.scss";
 styles;
 const Card = ({ card }: { card: Character[] }) => {
   return (
-    <Link to="/shopping-card" className={styles.container}>
+    <Link
+      to="/shopping-card"
+      className={styles.container}
+      onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+        const link = e.target as HTMLElement;
+        link.blur();
+      }}
+    >
       <div tabIndex={1} className={styles.wheeli}>
         <div className={styles.hours}>
           <span className={card.length < 10 ? styles.underTen : styles.moreTen}>{card.length}</span>
