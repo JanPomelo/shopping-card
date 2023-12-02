dbf0e094eaaf26db058e754aa270b5878d6c86a5
Jan Steikowski <j.steikowski@aol.com>
2023-12-02 10:56:10 +0700

    More styling of the shopping-card

--- src/components/Shopping-Card/ShoppingCard.tsx
+++ src/components/Shopping-Card/ShoppingCard.tsx
@@ -1,9 +1,8 @@
 "use strict";
-
-import Character from "../../types/character";
 import styles from "./ShoppingCard.module.scss";
+import ShoppingCardProps from "./ShoppingCardProps";
 
-const ShoppingCard = ({ card }: { card: Character[] }) => {
+const ShoppingCard: React.FC<ShoppingCardProps> = ({ card, plusClick, minusClick }) => {
   let total = 0;
   for (let i = 0; i < card.length; i++) {
     total += card[i].price! * card[i].hours!;
@@ -16,39 +15,45 @@ const ShoppingCard = ({ card }: { card: Character[] }) => {
           <h3>The Card is empty!</h3>Go back shopping!
         </div>
       ) : (
-        <>
-          {card.map((char) => {
-            return (
-              <div className={styles.charContainer} key={char.id}>
-                <img src={char.image} />
-                <h2>{char.name}</h2>
-                <div className={styles.hours}>
-                  <p>Hours:</p>
-                  <div>
-                    <button id={styles.minusButton}>-</button>
-                    <span>{char.hours}</span>
-                    <button id={styles.plusButton}>+</button>
+          <div className={styles.fullContainer}>
+          <>
+            {card.map((char) => {
+              return (
+                <div className={styles.charContainer} key={char.id}>
+                  <img src={char.image} />
+                  <h2>{char.name}</h2>
+                  <div className={styles.hours}>
+                    <p>Hours:</p>
+                    <div>
+                      <button className={styles.minusButton} id={card.indexOf(char) + "-Minus"} onClick={minusClick}>
+                        -
+                      </button>
+                      <span>{char.hours}</span>
+                      <button className={styles.plusButton} id={card.indexOf(char) + "-Plus"} onClick={plusClick}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className={styles.price}>
+                    <p>Price:</p>
+                    <div>
+                      <span>{char.hours! * char.price!}</span>
+                      <span className={styles.flurbo}></span>
+                    </div>
                   </div>
                 </div>
-                <div className={styles.price}>
-                  <p>Price:</p>
-                  <div>
-                    <span>{char.hours! * char.price!}</span>
-                    <span className={styles.flurbo}></span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </>
+              );
+            })}
+          </>
+          <div className={styles.total}>
+            <p>Total price:</p>
+            <p>
+              {total}
+              <span className={styles.flurbo}></span>
+            </p>
+          </div>
+        </div>
       )}
-      <div className={styles.total}>
-        <p>Total price:</p>
-        <p>
-          {total}
-          <span className={styles.flurbo}></span>
-        </p>
-      </div>
     </div>
   );
 };
